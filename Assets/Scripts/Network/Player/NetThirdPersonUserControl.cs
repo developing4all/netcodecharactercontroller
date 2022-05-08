ce30df56a552da68c3475fb2e04631b7292d9c1f
haydar <haydar@hosting4all.com>
2022-05-09 00:24:55 +0200

    Added new input system support and fixed bugs

--- Assets/Scripts/Network/Player/NetThirdPersonUserControl.cs
+++ Assets/Scripts/Network/Player/NetThirdPersonUserControl.cs
@@ -1,12 +1,12 @@
 using System;
 using UnityEngine;
-using UnityStandardAssets.CrossPlatformInput;
+using UnityEngine.InputSystem;
 using Cinemachine;
 using Unity.Netcode;
 
 namespace UnityStandardAssets.Characters.ThirdPerson
 {
-    [RequireComponent(typeof(NetThirdPersonCharacter))]
+    [RequireComponent(typeof(NetThirdPersonCharacter), typeof(PlayerInput))]
     public class NetThirdPersonUserControl : NetworkBehaviour
     {
         public Transform m_FollowTarget;
@@ -15,12 +15,25 @@ namespace UnityStandardAssets.Characters.ThirdPerson
         private Transform m_Cam;                  // A reference to the main camera in the scenes transform
         private Vector3 m_Move;
         private bool m_Jump;                      // the world-relative desired move direction, calculated from the camForward and user input.
+        private bool m_Crouch;
+
+        // The new input system
+        public PlayerInput playerInput;
+        private InputAction moveAction;
+        private InputAction lookAction;
+        private InputAction sprintAction;
+        private InputAction jumpAction;
+        private InputAction crouchAction;
+
+
         private CinemachineVirtualCameraBase m_VCam;
 
         public override void OnNetworkSpawn()
         {
+            //PlayerInput playerInput = GetComponent<PlayerInput>();
             if (!IsClient || !IsOwner)
             {
+                playerInput.enabled = false;
                 enabled = false;
                 return;
             }
@@ -29,40 +42,62 @@ namespace UnityStandardAssets.Characters.ThirdPerson
             m_VCam = m_Cam.GetComponent<CinemachineBrain>().ActiveVirtualCamera.VirtualCameraGameObject.GetComponent<CinemachineVirtualCameraBase>(); // this is fine since it only happens once.
             m_VCam.Follow = m_FollowTarget;
             m_VCam.LookAt = m_LookTarget;
-            /*// get the transform of the main camera
-            if (Camera.main != null)
-            {
-                m_Cam = Camera.main.transform;
-            }
-            else
-            {
-                Debug.LogWarning(
-                    "Warning: no main camera found. Third person character needs a Camera tagged \"MainCamera\", for camera-relative controls.", gameObject);
-                // we use self-relative controls in this case, which probably isn't what the user wants, but hey, we warned them!
-            }*/
+
+            // Added new input support
+            playerInput.enabled = true;
+            moveAction = playerInput.actions["Move"];
+            lookAction = playerInput.actions["Look"];
+            sprintAction = playerInput.actions["Sprint"];
+            jumpAction = playerInput.actions["Jump"];
+            crouchAction = playerInput.actions["Crouch"];
         }
 
         private void Start()
         {
             m_Character = GetComponent<NetThirdPersonCharacter>();
+            m_Crouch = false;
         }
 
         private void Update()
         {
+            // Prevent some issues if we run more than one client on the same machine
+            // by not getting input from non focussed game windows
+            if(!playerInput.enabled || !Application.isFocused ) return;
+
             if (!m_Jump)
             {
-                m_Jump = CrossPlatformInputManager.GetButtonDown("Jump");
+                m_Jump = jumpAction.triggered;
             }
+            CheckCrouch();
         }
 
+        private void CheckCrouch()
+        {
+            bool crouch = crouchAction.triggered;
 
+            // Are we already crouching? then stand up
+            if(crouch && m_Crouch)
+            {
+                m_Crouch = false;
+            }
+            // Otherwise crouch
+            else if(crouch)
+            {
+                m_Crouch = true;
+            }
+        }
+        
         // Fixed update is called in sync with physics
         private void FixedUpdate()
         {
+            // Prevent some issues if we run more than one client on the same machine
+            // by not getting input from non focussed game windows
+            if(!playerInput.enabled || !Application.isFocused) return;
+
             // read inputs
-            m_Move.x = CrossPlatformInputManager.GetAxis("Horizontal");
-            m_Move.z = CrossPlatformInputManager.GetAxis("Vertical");
-            bool crouch = Input.GetKey(KeyCode.C);
+            Vector2 moveIn = moveAction.ReadValue<Vector2>();
+            m_Move.x = moveIn.x;
+            m_Move.z = moveIn.y;
 
             m_Move.Normalize();
 
@@ -83,11 +118,11 @@ namespace UnityStandardAssets.Characters.ThirdPerson
             m_Move *= 0.5f;
 #if !MOBILE_INPUT
             // walk speed multiplier
-            if (Input.GetKey(KeyCode.LeftShift)) m_Move *= 2f;
+            if (sprintAction.IsPressed()) m_Move *= 2f;
 #endif
 
             // pass all parameters to the character control script
-            m_Character.Move(m_Move.x, m_Move.z, crouch, m_Jump);
+            m_Character.Move(m_Move.x, m_Move.z, m_Crouch, m_Jump);
             m_Jump = false;
         }
     }
